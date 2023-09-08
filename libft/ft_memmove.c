359495ce2652dda538fab9eb37f33a6e6c3f8998
Elizabeth <eteo@c2r5s14.42singapore.sg>
2023-09-08 12:43:54 +0800

    last changes in 42sg

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,35 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eteo <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/08 11:23:59 by eteo              #+#    #+#             */
-/*   Updated: 2023/09/08 12:33:16 by eteo             ###   ########.fr       */
+/*   Created: 2023/09/08 12:22:54 by eteo              #+#    #+#             */
+/*   Updated: 2023/09/08 12:31:11 by eteo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*memmove(void *dest, const void *src, size_t n)
 {
-	const char		*sce;
 	unsigned char	*dst;
-	size_t			i;
+	unsigned char	*buff;
+	const char	*sc;
+	size_t		i;
 
-	sce = src;
-	dst = dest;
 	i = 0;
-	if (!dest && !src)
-		return (NULL);
-	if (dst != sce)
-	{
-		while (i < n)
-		{
-			dst[i] = sce[i];
-			i++;
-		}
-	}
-	return (dest);
+	dst = dest;
+	sc = src;
+	while (i < n)
 }
