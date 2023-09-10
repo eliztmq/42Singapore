6ad0977cb1df2c9bb09512a1e849b9755658db8f
eliztmq <elizabethtmq@gmail.com>
2023-09-10 21:38:27 +0800

    unsaved changes

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,25 +6,25 @@
 /*   By: elizabethteo <elizabethteo@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 20:27:33 by elizabethte       #+#    #+#             */
-/*   Updated: 2023/09/10 20:31:53 by elizabethte      ###   ########.fr       */
+/*   Updated: 2023/09/10 21:36:40 by elizabethte      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-    char    *pt;
-    int     i;
+	char	*pt;
+	int		i;
 
-    i = 0;
-    pt = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
-    if (pt == NULL)
-        return (NULL);
-    while (s1[i] != '\0')
-    {
-        pt[i] = s1[i];
-        i++;
-    }
-    return (pt);
+	i = 0;
+	pt = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (pt == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		pt[i] = s1[i];
+		i++;
+	}
+	return (pt);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eteo <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: elizabethteo <elizabethteo@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 10:31:26 by eteo              #+#    #+#             */
-/*   Updated: 2023/09/09 18:17:04 by eteo             ###   ########.fr       */
+/*   Updated: 2023/09/10 21:38:15 by elizabethte      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,13 @@ int			ft_toupper(int c);
 int			ft_tolower(int c);
 int			ft_strncmp(const char *str1, const char *str2, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
+int			ft_atoi(const char *str);
 char		*ft_strchr(const char *str, int c);
 char		*ft_strrchr(const char *str, int c);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char		*ft_strdup(const char *str);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
 size_t		ft_strlen(const char *s);
 size_t		ft_strlcat(char *dest, const char *src, size_t size);
 size_t		ft_strlcpy(char *dest, const char *src, size_t size);
@@ -37,5 +41,6 @@ void		ft_bzero(void *s, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
+void		*ft_calloc(size_t count, size_t size);
 
 #endif
