5301c97ca9ae751f3127a47a5cb67865e5bd8a0d
eliztmq <elizabethtmq@gmail.com>
2023-09-21 18:01:48 +0800

    21092023

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elizabethteo <elizabethteo@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 17:17:04 by elizabethte       #+#    #+#             */
+/*   Updated: 2023/09/21 17:17:04 by elizabethte      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+
+#endif
