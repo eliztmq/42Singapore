1883fb2367cd1753138a807dc922f061a74994bb
Elizabeth <eteo@c2r6s15.42singapore.sg>
2023-09-22 18:02:59 +0800

    22092023.2

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,11 +6,29 @@
 /*   By: elizabethteo <elizabethteo@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 17:50:05 by elizabethte       #+#    #+#             */
-/*   Updated: 2023/09/22 12:39:19 by elizabethte      ###   ########.fr       */
+/*   Updated: 2023/09/22 18:02:45 by eteo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+int	ft_asgn(const char c, va_list *args)
+{
+	int	count;
+
+	count = 0;
+	if (c == 'c' || c == '%')
+		count += ft_putchar(va_arg(*args, unsigned int));
+	else if (c == 'x' || c == 'X')
+		count += ft_puthex(va_arg(*args, unsigned int));
+	else if (c == 'd' || c == 'i')
+		count += ft_putint(va_arg(*args, int));
+	else if (c == 's')
+		count += ft_putstr(va_arg(*args, char *));
+	else if (c == 'p')
+		count += ft_putptr(va_arg(*args, uintptr_t));
+	else if (c == 'u')	
+		
+}
 
 int	ft_printf(const char *str, ...)
 {
@@ -23,6 +41,10 @@ int	ft_printf(const char *str, ...)
 	va_start(args, str);
 	while (str[i])
 	{
-		
+		if (str[i] == '%')
+			ft_asgn(str[++i], &args);
+		else
+			write(1, format[i], 1);
+		i++;
 	}
 }
