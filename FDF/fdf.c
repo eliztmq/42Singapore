af94295bb3dc6830fcc44f916ea40b076b3f5a70
eliztmq <elizabethtmq@gmail.com>
2024-02-08 09:58:42 +0000

    080224 qn

--- FDF/fdf.c
+++ FDF/fdf.c
@@ -6,7 +6,7 @@
 /*   By: eteo <eteo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 17:13:13 by eteo              #+#    #+#             */
-/*   Updated: 2024/01/23 15:11:30 by eteo             ###   ########.fr       */
+/*   Updated: 2024/02/08 09:57:46 by eteo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,24 @@ void	my_mlx_pixel_put(t_data	*data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int main(void)
+void	error_msg(char *str)
 {
-	// int		i;
+	perror(str);
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv)
+{
 	t_data	img;
 	t_vars	vars;
+	int		fd;
 
-	// i = -1;
+	if (argc != 2)
+		error_msg("Incorrect inputs");
+	else
+		fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		error_msg("File Access");
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, 400, 400, "Hello world!!!");
 	img.img = mlx_new_image(vars.mlx, 1920, 1080);
@@ -51,6 +62,5 @@ int main(void)
 	mlx_hook(vars.win, 2, 1L<<0, key_hook, &vars);
 	mlx_hook(vars.win, 6, 1L<<6, mouse_hook, &vars);
 	mlx_loop(vars.mlx);
-	return (0);
 }
 
